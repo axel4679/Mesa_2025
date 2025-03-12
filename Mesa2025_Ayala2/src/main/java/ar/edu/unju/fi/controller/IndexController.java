9656a963876d1c4ee8e7f8f2ba64a049a1425d60
axelayala <axelcapofg@gmail.com>
2025-03-12 16:15:37 -0300

    En el inicio se deberían mostrar todos los boxes disponibles: implementado tambien mejore la vista del registro estacionamiento y automovil y reserva estacionamiento

--- Mesa2025_Ayala2/src/main/java/ar/edu/unju/fi/controller/IndexController.java
+++ Mesa2025_Ayala2/src/main/java/ar/edu/unju/fi/controller/IndexController.java
@@ -3,25 +3,27 @@ package ar.edu.unju.fi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.model.BoxDeEstacionamiento;
 import ar.edu.unju.fi.service.IBoxDeEstacionamientoService;
-import ch.qos.logback.core.model.Model;
-// GetMApping pedir informacion
-//Postmapping enviar informacion
+
+@Controller 
 public class IndexController {
   
   @Autowired
   private IBoxDeEstacionamientoService boxDeEstacionamientoService;
 
   @GetMapping("/")
-  public ModelAndView index(Model model) {
-           // Pasamos los datos al modelo
-      ModelAndView mav = new ModelAndView("index");
- 	 mav.addObject("espaciosDisponibles", boxDeEstacionamientoService.listar());
- 	 return mav;
+  public String index(Model model) {
+      List<BoxDeEstacionamiento> espaciosDisponibles = boxDeEstacionamientoService.listar();
+      
+      // Pasamos los datos al modelo
+      model.addAttribute("espaciosDisponibles", espaciosDisponibles);
+      
+      return "index"; // Retorna la vista "index.html"
   }
-  
 }
